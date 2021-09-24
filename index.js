a1376b48ad6e1da2db1f53d2b26dd8085f619f70
Roya Chobineh <roya.chbn@gmail.com>
2021-09-24 16:57:56 -0400

    LHL crypto function updated successfully

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+  get balance() {
+    // Calculate the balance using the transaction objects.
+  }
 
-  constructor(amount) {
-    this.amount = amount;
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+  commit() {
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
 
 }
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount
+  }
 
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
